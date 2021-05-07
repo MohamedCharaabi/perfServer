524d4003a45a54faf57afe24dc95c06cbc418be8
MohamedCharaabi <medch373@gmail.com>
2021-05-07 18:11:00 +0100

    add

--- models/theme.js
+++ models/theme.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 import { formerSchema } from './former.js';
 
+const ToySchema = new Schema({ name: String, ref: 'Former' });
+
 export const themeSchema = new mongoose.Schema({
     name: String,
     days: Number,
     formers: [
-        formerSchema //formers id
+        { ref: 'Former' }
     ],
 });
 
